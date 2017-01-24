3bb55b4970d259e49e0a049d29d887c57661334b
Aaron LeCheminant <Aaron@Brelyns-MacBook-Pro-2.local>
2017-01-24 13:06:10 -0500

    adding comments

---
+++ client/components/Comments.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+const Comments = React.createClass({
+
+   renderComment(comment, i){
+      console.log(comment);
+      return(
+         <div className="comment" key={i}>
+            <p>
+               <strong>{comment.user}</strong>
+               {comment.text}]
+               <button className="remove-comment">&times;</button>
+            </p>
+         </div>
+      )
+   },
+
+   render(){
+
+      return(
+         <div className="comment">
+            {this.props.postComments.map(this.renderComment)}
+            Im a comment
+         </div>
+      )
+   }
+
+})
+
+export default Comments;
--- client/components/single.js
+++ client/components/single.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments'
 
 const Single = React.createClass({
 
@@ -6,9 +8,19 @@ const Single = React.createClass({
 
 
    render(){
+      const { postId } = this.props.params;
+
+      const i = this.props.posts.findIndex((post) => post.code === postId);
+
+      console.log(i, 'EHHHYYY')
+      const postComments = this.props.comments[postId] || [];
+      //
+      const post = this.props.posts[i];
+
       return(
       <div className="single-photo">
-         the Single photo
+         <Photo i={i} post={post} {...this.props} />
+         <Comments postComments={postComments}/>
       </div>
       )
    }
